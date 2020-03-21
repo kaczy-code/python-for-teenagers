b3f62046c4a066e678299a2f0a0c7a92c6969d66
kaczy-code <56995969+kaczy-code@users.noreply.github.com>
2020-03-21 13:14:09 +0100

    Create Count10.py
     missing file Count10.py from Chapter 5

---
+++ Count10.py
@@ -0,0 +1,8 @@
+print("Sinister Loop, I know you are in there!")
+print("If you don't come out of the cafeteria freezer by the time I count to 10...")
+print("You won't get any of that delicious stop-sign shaped pizza!")
+
+for x in range(1,11):
+  print(x)
+
+print("I warned you! Now all the pizza belongs to Wonder Boy!")
