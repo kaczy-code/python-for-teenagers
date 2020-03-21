4b8d631f42a27508082aee54b40177c5101f155c
kaczy-code <56995969+kaczy-code@users.noreply.github.com>
2020-03-21 13:22:48 +0100

    Create DoomsdayClock.py
    missing file DoomsdayClock.py from Chapter 5

---
+++ DoomsdayClock.py
@@ -0,0 +1,17 @@
+print("The nefarious Sinister Loop stands before you, greedily rubbing his hands together!")
+print("He has his hand on a lever and has a large grin on his face.")
+print("Sinister Loop opens his mouth and says:")
+print("'You are doomed now Wonder Boy!'")
+print("'You have ten seconds to live! Listen as I count down the time!'")
+
+for x in range(10,0,-1):
+  print(x, "Mississippii!")
+
+# When x is equal to 5, print some text, then continue with the count down.
+  if x==5:
+    print("'Any last words, Wonder Boy?!?'")
+    continue
+
+print("You wait for your inevitable doom as the count reaches 0...")
+print("But nothing happens!")
+print("Sinister Loop screams, 'Foiled Again!'")
