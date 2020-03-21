88bd201bc2112baf06c56b0dc57ac2aa08ef923b
kaczy-code <56995969+kaczy-code@users.noreply.github.com>
2020-03-21 20:19:42 +0100

    Create InfiniteLoop.py
    missing file InfiniteLoop.py from Chapter 5

---
+++ InfiniteLoop.py
@@ -0,0 +1,10 @@
+# Create a variable with the value of 42.
+
+numberGuess = 42
+
+print("Sinister Loop stands before you!")
+print("Behold my infinite loop!")
+# create a while loop that continues while the value of numberGuess is 42.
+
+while numberGuess == 42:
+    print("Loop!")
