dc7607a9504d49eacefb2afa25c9b27de02b267c
kaczy-code <56995969+kaczy-code@users.noreply.github.com>
2020-03-21 12:58:45 +0100

    Create WonderBoyPassword.py
    missing file WonderBoyPassword.py from Chapter 5

---
+++ WonderBoyPassword.py
@@ -0,0 +1,12 @@
+# create a variable to hold Wonder Boy's password
+
+password = ''
+print("Welcome to Optimal Dad's Vault of Gadgets!")
+
+while password != "wonderboyiscool2018":
+  print("Please enter your password to access some fun tech!")
+  password = input()
+  
+print("You entered the correct password!")
+print("Please take whatever gadgets you need!")
+print("Don't touch the Doom Canon though - that belongs to Optimal Dad!")
