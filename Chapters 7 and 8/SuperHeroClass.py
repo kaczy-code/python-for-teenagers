e6e917350d188eb3f55982379f04642532d2f0bb
kaczy-code <56995969+kaczy-code@users.noreply.github.com>
2020-03-23 15:02:21 +0100

    super() and clean up

--- Chapters 7 and 8/SuperHeroClass.py
+++ Chapters 7 and 8/SuperHeroClass.py
@@ -1,8 +1,8 @@
-
 # Import the random module so we can randomly generate numbers
 # Import time module for dramatic pausing effect
 import random
 import time
+
 
 # Create a Superhero class that will act as a template for any heroes we create
 class Superhero():
@@ -18,18 +18,22 @@ class Superhero():
         self.dexterity = dexterity
         self.speed = speed
 
+
 # Adding random values to each stat using the random() function
-braun = random.randint(1,20)
-brains = random.randint(1,20)
-stamina = random.randint(1,20)
-wisdom = random.randint(1,20)
-constitution = random.randint(1,20)
-dexterity = random.randint(1,20)
-speed = random.randint(1,20)
+braun = random.randint(1, 20)
+brains = random.randint(1, 20)
+stamina = random.randint(1, 20)
+wisdom = random.randint(1, 20)
+constitution = random.randint(1, 20)
+dexterity = random.randint(1, 20)
+speed = random.randint(1, 20)
 
 # Creating a list of possible super powers
 
-superPowers = ['Flying', 'Super Strength', 'Telepathy', 'Super Speed', 'Can Eat a Lot of Hot Dogs', 'Good At Skipping Rope']
+superPowers = [
+    'Flying', 'Super Strength', 'Telepathy', 'Super Speed',
+    'Can Eat a Lot of Hot Dogs', 'Good At Skipping Rope'
+]
 
 # Randomly choosing a super power from the superPowers list
 # and assigning it to the variable power
@@ -38,38 +42,49 @@ power = random.choice(superPowers)
 
 # Creating lists of possible first and last names
 
-superFirstName = ['Wonder','Whatta','Rabid','Incredible', 'Astonishing', 'Decent', 'Stupendous', 'Above-average', 'That Guy', 'Improbably']
+superFirstName = [
+    'Wonder', 'Whatta', 'Rabid', 'Incredible', 'Astonishing', 'Decent',
+    'Stupendous', 'Above-average', 'That Guy', 'Improbably'
+]
 
-superLastName = ['Boy', 'Man', 'Dingo', 'Beefcake', 'Girl', 'Woman', 'Guy', 'Hero', 'Max', 'Dream', 'Macho Man','Stallion']
+superLastName = [
+    'Boy', 'Man', 'Dingo', 'Beefcake', 'Girl', 'Woman', 'Guy', 'Hero', 'Max',
+    'Dream', 'Macho Man', 'Stallion'
+]
 
 # Randomizing Super Hero Name
 # We do this by choosing one name from each of our two name lists
 # And adding it to the variable superName
 
-superName = random.choice(superFirstName)+ " " +random.choice(superLastName)
+superName = random.choice(superFirstName) + " " + random.choice(superLastName)
 
 # Creating a subclass of Superhero named Mutate
 # Mutate heroes will get a +10 bonus to their speed score.
 
+
 class Mutate(Superhero):
     def __init__(self):
-      Superhero.__init__(self)
-      print("You created a Mutate!")
-      self.speed = self.speed + 10
+        super().__init__()
+        print("You created a Mutate!")
+        self.speed = self.speed + 10
+
 
 # Creating a subclass of Superhero named Robot
 # Robot heroes will get a + 10 bonus to their braun score.
 
+
 class Robot(Superhero):
     def __init__(self):
-      Superhero.__init__(self)
-      print("You created a robot!")
-      self.braun = self.braun + 10
+        super().__init__()
+        print("You created a robot!")
+        self.braun = self.braun + 10
 
 
 # Introductory text
 
-print("Are you ready to create a super hero with the Super Hero Generator 3000?")
+print(
+    "Are you ready to create a super hero with the Super Hero Generator 3000?"
+)
 
 # Ask the user a question and prompt them for an answer
 # input() 'listens' to what they type on their keyboard
@@ -78,7 +93,7 @@ print("Are you ready to create a super hero with the Super Hero Generator 3000?"
 print("Enter Y/N:")
 
 answer = input()
-answer = (answer.upper())
+answer = answer.upper()
 
 # While loop to check for the answer "YES"
 # This loop will continue while the value of answer IS NOT "YES"
@@ -88,7 +103,7 @@ while answer != "Y":
     print("I'm sorry, but you have to choose Y to continue!")
     print("Choose Y/N:")
     answer = input()
-    answer = (answer.upper())
+    answer = answer.upper()
 
 print("Great, let's get started!")
 
@@ -99,8 +114,7 @@ print("Press 2 for a Mutate Superhero")
 print("Press 3 for a Robot Superhero")
 answer2 = input()
 
-
-if answer2=='1':
+if answer2 == '1':
 
     # Creating the Superhero object
     hero = Superhero()
@@ -114,16 +128,16 @@ if answer2=='1':
     for i in range(1):
         print("...........")
         time.sleep(3)
-    
+
         print("(nah...you wouldn't like THAT one...)")
 
     for i in range(2):
         print("...........")
         time.sleep(3)
-    
+
     print("(almost there....)")
     print(" ")
-    print("Your name is %s." % (hero.superName))
+    print("Your name is %s." % hero.superName)
     print("Your super power is: ", hero.power)
     print("Your new stats are:")
     print("")
@@ -136,66 +150,68 @@ if answer2=='1':
     print("Speed ", hero.speed)
     print("")
 
-elif answer2=='2':
-        # Creating a Mutate object
-        hero2 = Mutate()
-        print("Generating stats, name, and super powers.")
+elif answer2 == '2':
+    # Creating a Mutate object
+    hero2 = Mutate()
+    print("Generating stats, name, and super powers.")
 
     # Creating dramatic effect
 
-        for i in range(1):
-            print("...........")
-            time.sleep(3)
-    
-            print("(nah...you wouldn't like THAT one...)")
+    for i in range(1):
+        print("...........")
+        time.sleep(3)
 
-        for i in range(2):
-            print("...........")
-            time.sleep(3)
-        
-        print("Your name is %s." % (hero2.superName))
-        print("Your super power is: ", hero2.power)
-        print("Your new stats are:")
-        print("")
-        print("Brains: ", hero2.brains)
-        print("Braun: ", hero2.braun)
-        print("Stamina: ", hero2.stamina)
-        print("Wisdom: ", hero2.wisdom)
-        print("Constitution: ", hero2.constitution)
-        print("Dexterity: ", hero2.dexterity)
-        print("Speed ", hero2.speed)
-        print("")
-
-elif answer2=='3':
-        # Create a Robot character
-
-        hero3 = Robot()
-
-        print("Generating stats, name, and super powers.")
-
-        # Creating dramatic effect
-
-        for i in range(1):
-            print("...........")
-            time.sleep(3)
-    
         print("(nah...you wouldn't like THAT one...)")
 
-        for i in range(2):
-            print("...........")
-            time.sleep(3)
+    for i in range(2):
+        print("...........")
+        time.sleep(3)
 
-        print("Your name is %s." % (hero3.superName))
-        print("Your super power is: ", hero3.power)
-        print("Your new stats are:")
-        print("")
-        print("Brains: ", hero3.brains)
-        print("Braun: ", hero3.braun)
-        print("Stamina: ", hero3.stamina)
-        print("Wisdom: ", hero3.wisdom)
-        print("Constitution: ", hero3.constitution)
-        print("Dexterity: ", hero3.dexterity)
-        print("Speed ", hero3.speed)
-        print("")
+    print("Your name is %s." % hero2.superName)
+    print("Your super power is: ", hero2.power)
+    print("Your new stats are:")
+    print("")
+    print("Brains: ", hero2.brains)
+    print("Braun: ", hero2.braun)
+    print("Stamina: ", hero2.stamina)
+    print("Wisdom: ", hero2.wisdom)
+    print("Constitution: ", hero2.constitution)
+    print("Dexterity: ", hero2.dexterity)
+    print("Speed ", hero2.speed)
+    print("")
+
+elif answer2 == '3':
+    # Create a Robot character
+
+    hero3 = Robot()
+
+    print("Generating stats, name, and super powers.")
+
+    # Creating dramatic effect
+
+    for i in range(1):
+        print("...........")
+        time.sleep(3)
+
+    print("(nah...you wouldn't like THAT one...)")
+
+    for i in range(2):
+        print("...........")
+        time.sleep(3)
+
+    print("Your name is %s." % hero3.superName)
+    print("Your super power is: ", hero3.power)
+    print("Your new stats are:")
+    print("")
+    print("Brains: ", hero3.brains)
+    print("Braun: ", hero3.braun)
+    print("Stamina: ", hero3.stamina)
+    print("Wisdom: ", hero3.wisdom)
+    print("Constitution: ", hero3.constitution)
+    print("Dexterity: ", hero3.dexterity)
+    print("Speed ", hero3.speed)
+    print("")
 else:
-        print("You did not choose the proper answer! Program will now self-destruct!")
+    print(
+        "You did not choose the proper answer! Program will now self-destruct!"
+    )
