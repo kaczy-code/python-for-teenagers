b18b84565794a57a623ea4ede438b26dd4c5865f
kaczy-code <56995969+kaczy-code@users.noreply.github.com>
2020-03-23 14:02:37 +0100

    Add files via upload

---
+++ Chapters 7 and 8/SampleClassandObject.py
@@ -0,0 +1,11 @@
+class Superhero():
+    def fly(self):
+        print("Look at me, I'm so fly!")
+
+    def hotDog(self):
+        print("I sure do like hot dogs!")
+
+
+HotDogMan = Superhero()
+HotDogMan.fly()
+HotDogMan.hotDog()
